05131e574c854f9ebcfd9cdc30d88a46441c0225
sachecka <checkaseth@gmail.com>
2020-03-22 09:21:53 -0400

    3/22/2020 - Working code. Need to create Pandas data frame

--- GetData.py
+++ GetData.py
@@ -4,22 +4,32 @@ import pandas as pd
 from TickersList import tickers
 import re
 
+# Column names that are needed in the pandas dataframe
 column_names = ['Ticker','Company Name','BusinessType','Date','Open','High','Low','Beta',
                 'VWAP','Market Cap All Classes', 'Dividend',
                 'Div Freq', 'P/E Ratio', 'EPS','Yield',
                 'P/B Ratio','Exchange']
 
-# df = pd.DataFrame(columns = column_names)
-
+# Create dictionary and fill with empty lists
 tickerDict = {}
 for i in column_names:
     tickerDict[i]=[None]*len(tickers)
 
+# Extract the data from web for each ticker
 for i in range(len(tickerDict['Ticker'])):
     tickerDict['Ticker'][i]=tickers[i]
     results = requests.get('https://web.tmxmoney.com/quote.php?qm_symbol=' + tickers[i])
     src = results.content
     soup = BeautifulSoup(src, 'lxml')
+    texts = soup.find_all(text=True)
+    # Get company name
+    compName = soup.find_all('div', class_="quote-company-name")
+    tickerDict['Company Name'][i] = compName[0].find('h4').text
+    # Get the date
+    dateTick = compName[0].find('p').text
+    dateTick = dateTick.split('|')
+    tickerDict['Date'][i] = re.sub('\s', '', dateTick[1])
+    # most of the info is in the dq_card div class
     dq_card = soup.find_all('div', class_="dq-card")
     for dq in range(len(dq_card)):
         if 'Open' in dq_card[dq].text :
@@ -32,9 +42,11 @@ for i in range(len(tickerDict['Ticker'])):
             tickerDict['Beta'][i] = dq_card[dq].find('strong').string
         if 'All Classes' in dq_card[dq].text :
             tickerDict['Market Cap All Classes'][i] = dq_card[dq].find('strong').string
-        if 'DIVIDEND' in dq_card[dq].text :
-            tickerDict['Dividend'][i] = dq_card[dq].find('strong').string
-        if 'DIV. FREQUENCY' in dq_card[dq].text :
+        if 'Dividend' in dq_card[dq].text :
+            dividend = dq_card[dq].find('strong').string
+            dividend = dividend.replace("\xa0", " ")
+            tickerDict['Dividend'][i] = dividend
+        if 'Div. Frequency' in dq_card[dq].text :
             tickerDict['Div Freq'][i] = dq_card[dq].find('strong').string
         if 'P/E Ratio' in dq_card[dq].text :
             tickerDict['P/E Ratio'][i] = dq_card[dq].find('strong').string
--- Test.py
+++ Test.py
@@ -14,13 +14,13 @@ soup = BeautifulSoup(src, 'lxml')
 
 dq_card = soup.find_all('div', class_= "dq-card")
 
-print(dq_card)
-print(dq_card[14].find('strong').string)
-print(dq_card[14].text)
-print(len(dq_card))
+#print(dq_card)
+#print(dq_card[14].find('strong').string)
+#print(dq_card[14].text)
+#print(len(dq_card))
 
-if 'High' in dq_card[1].text:
-    print(dq_card[1].find('strong').string)
+#if 'High' in dq_card[1].text:
+#    print(dq_card[1].find('strong').string)
 
 column_names = ['Ticker','Company Name','Date','Open','High','Low','Beta',
                 'VWAP','Market Cap','Market Cap All Classes', 'Dividend',
@@ -29,8 +29,14 @@ column_names = ['Ticker','Company Name','Date','Open','High','Low','Beta',
 
 # df = pd.DataFrame(columns = column_names)
 
-tickerDict = {}
-for i in column_names:
-    tickerDict[i]=[None]*len(tickers)
+#tickerDict = {}
+#for i in column_names:
+#    tickerDict[i]=[None]*len(tickers)
 
 #print(len(tickerDict['Ticker']))
+
+compName = soup.find_all('div', class_= "quote-company-name")
+x = compName[0].find('p').text
+x=x.split('|')
+print(x[1])
+
